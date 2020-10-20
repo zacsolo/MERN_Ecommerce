3f9e7ca5c868aab1867804c51dd8c5c9f25e0d95
Zac Solomon <zac@zacsolo.com>
2020-10-20 15:46:20 -0400

    React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,18 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' exact component={HomeScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
