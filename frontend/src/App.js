c02cae98bd8b10fe7f4105855ea8e7165a5bfed4
Zac Solomon <zac@zacsolo.com>
2020-10-20 16:11:43 -0400

    prodcut screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 const App = () => {
   return (
     <Router>
@@ -11,6 +12,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/' exact component={HomeScreen} />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
