1963642768e7972be314fa39452a028b36062eb8
Zac Solomon <zac@zacsolo.com>
2020-10-21 15:10:29 -0400

    User Model

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+import connectDB from './config/db.js';
+import colors from 'colors';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -15,4 +22,11 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
