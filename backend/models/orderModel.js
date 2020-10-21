9d3f47ee732185fe75027a84dee180b89a13ff4a
Zac Solomon <zac@zacsolo.com>
2020-10-21 16:36:27 -0400

    Data Seeder

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -73,6 +73,6 @@ const orderSchema = mongoose.Schema(
   }
 );
 
-const Order = mongoose.Model('Order', orderSchema);
+const Order = mongoose.model('Order', orderSchema);
 
 export default Order;
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -72,6 +72,6 @@ const productSchema = mongoose.Schema(
   }
 );
 
-const Product = mongoose.Model('Product', productSchema);
+const Product = mongoose.model('Product', productSchema);
 
 export default Product;
