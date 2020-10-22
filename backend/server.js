607363f594fd42596af8376ff18646a5754150ec
Zac Solomon <zac@zacsolo.com>
2020-10-22 10:05:07 -0400

    install redux and create store

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errormiddleware.js';
 
 dotenv.config();
 
@@ -15,6 +16,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
