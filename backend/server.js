aa5af04b1e39d0b9ff738cc452fa60b2581bd25e
Zac Solomon <zac@zacsolo.com>
2020-10-29 17:04:51 -0400

    server, fetching orders by ID

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/orders', orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
