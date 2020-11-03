987c3f9d82df28f8b1593b9140754fa6fbe8dfcf
Zac Solomon <zac@zacsolo.com>
2020-11-03 13:13:52 -0500

    Reset userList on logout and redirect to home

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,6 +16,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from '../constants/userConstants';
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 import axios from 'axios';
@@ -53,6 +54,7 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: USER_LIST_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
 };
 
