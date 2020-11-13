5f55bb0fd540e005f314a5af5bfa9ea4625c3050
Zac Solomon <zac@zacsolo.com>
2020-11-13 10:19:09 -0500

    Product Review Routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   createProduct,
+  createProductReview,
   deleteProduct,
   getProductById,
   getProducts,
@@ -16,5 +17,6 @@ router
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
